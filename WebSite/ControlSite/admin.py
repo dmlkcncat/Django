3aa011ffe3505bbca85e5fb07215d6dbc75866e1
dmlkcncat <67742057+dmlkcncat@users.noreply.github.com>
2021-02-26 11:35:26 +0300

    Add files via upload

---
+++ WebSite/ControlSite/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from ControlSite.models import Sube, Personel, YillikIzin, FazlaMesai, SistemTablosu, Yonetici, Mazeret
+
+admin.site.register(Sube)
+admin.site.register(Personel)
+admin.site.register(YillikIzin)
+admin.site.register(FazlaMesai)
+admin.site.register(SistemTablosu)
+admin.site.register(Yonetici)
+admin.site.register(Mazeret)
+
+
+
+
+# Register your models here.
---
+++ WebSite/ControlSite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ControlsiteConfig(AppConfig):
+    name = 'ControlSite'
